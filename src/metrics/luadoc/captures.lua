38d18b0195561e41552d97801bfde3c8b2e3219d
Peter Kosa <kosa.peter24@gmail.com>
2013-06-09 23:27:17 +0200

    Added: -descriptions for tables(assigns) -bugfix? see line --bug and line --?

--- src/metrics/luadoc/captures.lua
+++ src/metrics/luadoc/captures.lua
@@ -11,6 +11,7 @@ local io, table, pairs, type, print,string = io, table, pairs, type, print,strin
 module ('metrics.luadoc.captures')
 
 
+
 local stack_functions							-- zoznam funkcii (tabulka dictionary) ktora je neskor vratena
 local stack_tables
 local funcs = {}
@@ -26,18 +27,19 @@ local function processFunction(comment, funcAST)
 	else
 		return
 	end
-
-	local result = commentParser.parse(comment)
+	local result = commentParser.parse(comment,0,true) or commentParser.parse(comment,0) 
 	if (result) then
 		funcAST.documented=1
 		for k,v in pairs(result) do
 			if (v.tag == 'comment') then
-				funcAST.comment =(funcAST.comment or '') ..  string.gsub(v.text,'.',string.upper,1) .. ' , ' 			
+
+				funcAST.description =(funcAST.description or '') .. v.text .. ' ' 			
 			end
 			if (v.item == 'name') then 
 				name = v.text
 			end
 		end
+		funcAST.comment=(string.match(funcAST.description, "(.-%.)[%s\t]") or funcAST.description)
 	end	
 	
 	local block = nil
@@ -80,20 +82,22 @@ local function processAssign(comment, assignAST)
 	local ldoc_name = nil
 	local commentflag = 0
 	local comment = ''
-		
+	local description=""
 	if (result) then
+
 		for k,v in pairs(result) do
 			if (v.item == 'name') then
 				ldoc_name = v.text
 			end
 			if (v.tag == 'comment') then
-				comment =comment  ..  string.gsub(v.text,'.',string.upper,1) .. ' , '
+				description =(description or '') .. v.text .. ' ' 	
 			end
 			
 			if (v.item == 'class') then
 				ldoc_class = v.text
 			end
 		end
+		comment=(string.match(description, "(.-%.)[%s\t]") or description)
 	end	
 
 		
@@ -107,11 +111,20 @@ local function processAssign(comment, assignAST)
 			
 		end			
 				
-		for k,v in pairs(namelist.data) do 			-- compare namelist ane explist values ... create resulting table
+		for k,v in pairs(namelist.data) do 			-- compare namelist and explist values ... create result table
 			if (v.text == ldoc_name) then
-			explist.data[k].documented = commentflag
-			explist.data[k].comment = comment
-			stack_tables[v.text] = explist.data[k]
+				explist.data[k].documented = commentflag
+				explist.data[k].description = description
+				explist.data[k].comment = comment
+				if(assignAST.tag == 'Assign')then
+					explist.data[k].ttype = 	''		--old: 'global' not sure, what if  
+														-- local newtable 
+														-- newtable = {}
+														--TODO set correct ttype -- see: captures/block.lua 
+				else
+					explist.data[k].ttype = 	'local'
+				end
+				stack_tables[v.text] = explist.data[k]
 				break
 			end
 		end
@@ -134,15 +147,27 @@ captures = {
 			local searchNode = fun
 	
 			if (fun.assignNode) then searchNode=fun.assignNode end
+--bug util.getComment function returns all comments and empty lines before the node,what if the first line isn't the luadoc-style comment (not starting with ---)
 			local comment = utils.getComment(searchNode)
 			fun.documented = 0
-			if (comment) then processFunction(comment, fun)	end	
+			if (comment) then 
+--? is this correct solution for bug above?
+				comment=string.match(comment,"%-%-%-.*")
+				if(comment)then
+					processFunction(comment, fun)	
+				end
+			end	
 		end
 		
 		for k,tab in pairs(tabs) do
 			local comment = utils.getComment(tab)
 			tab.documented = 0
-			if (comment) then processAssign(comment, tab)	end	
+			if (comment) then 
+				comment=string.match(comment,"%-%-%-.*")
+				if(comment)then
+					 processAssign(comment, tab)	
+				end
+			end	
 		end
 		
 		-- made work dith current luadoc taglet

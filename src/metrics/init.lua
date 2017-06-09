8ba59a30ae7f32361baf0cf0f55e5f7afbd8d2a1
Martin Nagy <mnagy112@gmail.com>
2017-06-09 17:26:09 +0200

    Added comments, moved CSS and JS files to share/ and 3rdparty/ folders

--- src/metrics/init.lua
+++ src/metrics/init.lua
@@ -256,12 +256,12 @@ function doGlobalMetrics(file_metricsAST_list)
 	end		
   
   --merge code smells
-  returnObject.documentSmells = {}
-  returnObject.documentSmells.MI = smells_capt.countMI(file_metricsAST_list)
-  returnObject.documentSmells.functionSmells = smells_capt.countFunctionSmells(file_metricsAST_list)
-  returnObject.documentSmells.moduleSmells = {}
+  returnObject.documentSmells = {} --Document smells sub-table
+  returnObject.documentSmells.MI = smells_capt.countMI(file_metricsAST_list) --Add maintainability index
+  returnObject.documentSmells.functionSmells = smells_capt.countFunctionSmells(file_metricsAST_list) --Add function smells eg: LOC, cyclomatic, halstead etc.
+  returnObject.documentSmells.moduleSmells = {} --Module smells sub-table
   
-  for filename, AST in pairs(file_metricsAST_list) do
+  for filename, AST in pairs(file_metricsAST_list) do --Merge smells in modules to sub-table
     table.insert(returnObject.documentSmells.moduleSmells, {file = filename, RFC = AST.smells.RFC, WMC = AST.smells.WMC, NOM = AST.smells.NOM, responseToNOM = AST.smells.responseToNOM, CBO = AST.smells.CBO})
   end
 
